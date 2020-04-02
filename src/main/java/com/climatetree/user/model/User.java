06a27844580668cdfb5a90493a0f71d846800359
unknown <shwetamandavgane@northeastern.edu>
2020-04-02 16:18:16 -0700

    Changed UserController file

--- src/main/java/com/climatetree/user/model/User.java
+++ src/main/java/com/climatetree/user/model/User.java
@@ -12,6 +12,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+/**
+ * The type User.
+ */
 /*
 Object representation of the USER table from the database. Setters and getters + constructors.
  */
@@ -45,9 +48,17 @@ public class User {
 	@Column(name = "last_login_location")
 	private String lastLoginLocation;
 
+	/**
+	 * Instantiates a new User.
+	 */
 	public User() {
 	}
 
+	/**
+	 * Instantiates a new User.
+	 *
+	 * @param user the user
+	 */
 	public User(User user) {
 		this.email = user.getEmail();
 		this.nickname = user.getNickname();
@@ -57,71 +68,157 @@ public class User {
 		this.registrationDate = user.registrationDate;
 	}
 
+	/**
+	 * Instantiates a new User.
+	 *
+	 * @param email    the email
+	 * @param nickname the nickname
+	 */
 	public User(String email, String nickname) {
 		this.email = email;
 		this.nickname = nickname;
 	}
 
+	/**
+	 * Gets user id.
+	 *
+	 * @return the user id
+	 */
 	public Long getUserId() {
 		return userId;
 	}
 
+	/**
+	 * Sets user id.
+	 *
+	 * @param userId the user id
+	 */
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
 
+	/**
+	 * Gets email.
+	 *
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Sets email.
+	 *
+	 * @param email the email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * Gets nickname.
+	 *
+	 * @return the nickname
+	 */
 	public String getNickname() {
 		return nickname;
 	}
 
+	/**
+	 * Sets nickname.
+	 *
+	 * @param nickname the nickname
+	 */
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
 
+	/**
+	 * Gets blacklisted.
+	 *
+	 * @return the blacklisted
+	 */
 	public Boolean getBlacklisted() {
 		return blacklisted;
 	}
 
+	/**
+	 * Sets blacklisted.
+	 *
+	 * @param blacklisted the blacklisted
+	 */
 	public void setBlacklisted(Boolean blacklisted) {
 		this.blacklisted = blacklisted;
 	}
 
+	/**
+	 * Gets role.
+	 *
+	 * @return the role
+	 */
 	public Role getRole() {
 		return role;
 	}
 
+	/**
+	 * Sets role.
+	 *
+	 * @param role the role
+	 */
 	public void setRole(Role role) {
 		this.role = role;
 	}
 
+	/**
+	 * Gets registration date.
+	 *
+	 * @return the registration date
+	 */
 	public Date getRegistrationDate() {
 		return registrationDate;
 	}
 
+	/**
+	 * Sets registration date.
+	 *
+	 * @param registrationDate the registration date
+	 */
 	public void setRegistrationDate(Date registrationDate) {
 		this.registrationDate = registrationDate;
 	}
 
+	/**
+	 * Gets last login time.
+	 *
+	 * @return the last login time
+	 */
 	public Date getLastLoginTime() {
 		return lastLoginTime;
 	}
 
+	/**
+	 * Sets last login time.
+	 *
+	 * @param lastLoginTime the last login time
+	 */
 	public void setLastLoginTime(Date lastLoginTime) {
 		this.lastLoginTime = lastLoginTime;
 	}
 
+	/**
+	 * Gets last login location.
+	 *
+	 * @return the last login location
+	 */
 	public String getLastLoginLocation() {
 		return lastLoginLocation;
 	}
 
+	/**
+	 * Sets last login location.
+	 *
+	 * @param lastLoginLocation the last login location
+	 */
 	public void setLastLoginLocation(String lastLoginLocation) {
 		this.lastLoginLocation = lastLoginLocation;
 	}
